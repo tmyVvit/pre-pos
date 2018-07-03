6966e601dfdf4021708ae904b41b23f30948aa4c
terry.teng@oocl.com <Ilovetmyy1>
2018-07-03 19:23:17 +0800

    [Terry] section 2 practice 2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,27 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var ans = new Array();
+  var table = new Array();
+  var ch, count;
+  for(var i = 0; i < collection.length; i++){
+    if(collection[i].length > 1){
+      ch = collection[i][0];
+      count = collection[i][2]; 
+    } else {
+      ch = collection[i];
+      count = 1;
+    }
+    var index = table.indexOf(ch);
+    if(index > -1){
+      ans[index].count++;
+    }else {
+      table.push(ch);
+      var tmp = new Object();
+      tmp.key = ch;
+      tmp.count = count*1;
+      ans.push(tmp);
+    }
+  }
+  return ans;
 }
