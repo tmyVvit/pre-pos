e7c9e060de74d4caac0d4fdbb69a06412182d4da
terry.teng@oocl.com <Ilovetmyy1>
2018-07-03 19:46:40 +0800

    [Terry] section 3 practice 3

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,27 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var c = new Array();
+  var table = new Array();
+  for(var i = 0; i < collectionA.length; i++){
+    var index = table.indexOf(collectionA[i]);
+    if(index > -1){
+      c[index].count++;
+    }else {
+      table.push(collectionA[i]);
+      var tmp = new Object();
+      tmp.key = collectionA[i];
+      tmp.count = 1;
+      c.push(tmp);
+    }
+  }
+  var oc = objectB.value.toString();
+  for(var i = 0; i < c.length; i++) {
+    if(oc.indexOf(c[i].key.toString())>-1)
+      if(c[i].count >= 3){
+        c[i].count -= Math.floor(c[i].count/3);
+      }
+    }
+  
+  return c;
 }
