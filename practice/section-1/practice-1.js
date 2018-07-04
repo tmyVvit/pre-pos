2154b31196a809ae90d9f6966dadc7a4b03f9863
terry.teng@oocl.com <Ilovetmyy1>
2018-07-04 11:08:48 +0800

    [Terry] s1p1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,7 +2,7 @@
 
 function collectSameElements(collectionA, collectionB) {
   var ans = new Array();
-  var c = collectionB.toString();
+  var c = collectionB.value.toString();
   for(var i = 0; i < collectionA.length; i++){
     if(c.indexOf(collectionA[i].toString())>-1){
       ans.push(collectionA[i]);
